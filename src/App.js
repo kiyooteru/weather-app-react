4313f28b87ce985d1c731affd743c8e3b2ed80e7
kiyooteru <kiyooteru@gmail.com>
2022-12-16 13:51:40 +0100

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by Kinga Miśkiewicz and is{" "}
+        <a
+          href="https://github.com/kiyooteru/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
