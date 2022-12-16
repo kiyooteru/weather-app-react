fea349ec10616ecffbfdc69d25ad16e5382ce330
kiyooteru <kiyooteru@gmail.com>
2022-12-16 14:56:57 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by Kinga Miśkiewicz and is{" "}
-        <a
-          href="https://github.com/kiyooteru/weather-app-react"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather app
+        <footer>
+          This project was coded by Kinga Miśkiewicz and is{" "}
+          <a
+            href="https://github.com/kiyooteru/weather-app-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
